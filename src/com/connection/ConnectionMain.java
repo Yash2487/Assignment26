139d04acad28c7e23ecd5947c5f170f633a742a2
Yash Mate <yash.mate4494@gmail.com>
2023-04-12 22:53:29 +0530

    assignment26

---
+++ src/com/connection/ConnectionMain.java
@@ -0,0 +1,23 @@
+package com.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionMain {
+
+	// create a method which returns connection
+	public Connection getConnection() {
+		Connection connection = null;
+		try {
+			// step1- loading Driver class
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// step2- establishing connection
+			connection = DriverManager.getConnection("mysql:jdbc://localhost:3306/assignment26", "root",
+					"Yash_Mate@#2487");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
---
+++ src/com/insert/values/into/table/InsertSingleData.java
@@ -0,0 +1,10 @@
+package com.insert.values.into.table;
+
+public class InsertSingleData {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
