e57750adc4dbd64e8f770258543d8985f24aa6ba
Yash Mate <yash.mate4494@gmail.com>
2023-04-12 23:25:00 +0530

    update JDBC connection

--- src/com/connection/ConnectionMain.java
+++ src/com/connection/ConnectionMain.java
@@ -13,7 +13,7 @@ public class ConnectionMain {
 			Class.forName("com.mysql.jdbc.Driver");
 
 			// step2- establishing connection
-			connection = DriverManager.getConnection("mysql:jdbc://localhost:3306/assignment26", "root",
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/assignment26", "root",
 					"Yash_Mate@#2487");
 		} catch (Exception e) {
 			e.printStackTrace();
